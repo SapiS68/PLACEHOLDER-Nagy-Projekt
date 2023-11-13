8701356f28560843ecd560c54347576a6e50f845
SapiS68 <cz.sapis@gmail.com>
2023-11-13 19:44:08 +0100

    Bejelentkezéshez kapcsolódó adatbázis migrációs fájlok létrehozása

--- Projekt/database/migrations/2023_11_13_182122_name_of_the_game_create_users_table.php
+++ Projekt/database/migrations/2023_11_13_182122_name_of_the_game_create_users_table.php
@@ -12,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->primary();
+            $table->integer('role_id');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('email')->unique();
+
+            $table->foreign('role_id')->references('role_id')->on('roles');
         });
     }
 
