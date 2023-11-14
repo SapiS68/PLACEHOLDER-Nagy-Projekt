6f14df0e514f096c15d734ada0000d6ff5cf0de8
Törteli Márton <60424745+ewyyn7@users.noreply.github.com>
2023-11-14 20:53:55 +0100

    Register oldal felépítése.
    Textbox és form elkészítése. Formba method, inputba pedig a namek helyes adatokkal feltöltése. CSS formázás.

--- Projekt/resources/views/layouts/base.blade.php
+++ Projekt/resources/views/layouts/base.blade.php
@@ -11,22 +11,49 @@
     <div class="w-5/6 h-1/2 p-10 ">
         <div class="items-center justify-center">
             <div class="bg-neutral-950 p-3 rounded-lg text-gray-50">
-                    <h1 class="	text-transform: uppercase text-bold font-bold mb-1">textbox</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum nobis nam cum, corporis deleniti sequi fuga quod soluta amet optio similique illum volupta Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, assumenda quibusdam nostrum maiores voluptas nisi odit quam ratione ipsa explicabo aperiam libero distinctio possimus aspernatur dolores minima quia eum minus. tes! Tempore consequuntur ut distinctio ducimus repellendus optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus alias, ea blanditiis ex neque esse fugiat cumque pariatur debitis amet libero aspernatur labore omnis deleniti aperiam. Voluptatibus id quia adipisci.</p>
+                    <h1 class="flex justify-center	text-transform: uppercase text-bold font-bold mb-1">a játékról röviden...</h1>
+                    <p>Ez egy dinamikus webes platform, ahol a videojátékok világába kalandozva tesztelheted tudásodat. 
+Mint a framed.wtf követője, kérdéseket és képeket kínál, amelyek segítségével kihívások elé nézhetsz, 
+és versenyre kelhetsz más játékosokkal. Minden nap új videojáték nevekkel és hozzájuk kapcsolódó képekkel 
+találkozhatsz. Az adminisztrátorok által létrehozott tartalmak garantálják az izgalmakat.
+
+Válaszaidat egyszerűen és gyorsan rögzítheted, és azonnal láthatod, hogy hol 
+helyezkedsz el az aktuális ranglistán.
+Kövesd nyomon a válaszaidra fordított időt és a megtekintett képek számát. 
+Az automatikusan frissülő statisztikák segítenek nyomon követni a fejlődésedet.
+A korábbi kihívások, valamint a hozzájuk kapcsolódó képek archiválva maradnak, így 
+bármikor visszanézve átélheted a múlt pillanatait.</p>
+            
+                    <div class="flex justify-center p-5 m-5">
+                        <img class="w-1/3 h-1/3 rounded-lg"src="https://i.imgur.com/VDSFO47.png" >
+                    </div>  
+                    
             </div>
         </div>
     </div>
 
-    <div class="w-1/2 h-1/2 p-10 ">
-        <div class="items-center justify-center">
+    <div class="w-1/5 h-1/2 p-10 ">
+        <div>
             <div class="bg-neutral-950 p-3 rounded-lg text-gray-50">
-                <h1 class="	text-transform: uppercase text-bold font-bold mb-1">reg</h1>
-                <form class="space-y-6" action="#" method="POST">
-                    <input class="bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name="username" type="text" placeholder="input line" /> 
+                <h1 class="flex justify-center text-transform: uppercase text-bold font-bold mb-1">regisztrálj!</h1>
 
+                <form class="space-y-6" action="/register" method="POST">
+                    @csrf
+                    <input  class=" bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name="username" type="text" placeholder="Felhasználónév" /> 
+                    <input  class=" bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name="email" type="text" placeholder="Email" /> 
+                    <input  class=" bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name="password" type="password" placeholder="Jelszó" />                 
+                    
+                    <input  class=" bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name="password_confirmation" type="password" placeholder="Jelszó ismét" />                     
+                    <div class="flex justify-center">
+                        <button class="flex justify-center text-slate-950 m-3 p-1 rounded bg-gradient-to-r from-green-500 to-emerald-500 hover:from-green-600 hover:to-emerald-600 font-semibold">Ok</button>
+                    </div>
+                    
                 </form>
-                <button class="text-slate-950 m-1 p-1 rounded bg-gradient-to-r from-green-500 to-emerald-500 hover:from-green-600 hover:to-emerald-600 font-semibold">zöld gomb</button>
 
+                <div class="flex justify-center">
+                    <a href="/login"class=" text-slate-950 m-3 p-1 rounded bg-gradient-to-r from-red-600 to-amber-400 hover:from-red-700 hover:to-orange-500 font-semibold">Van fiókom</a>
+                    
+                </div>
             </div>
         </div>
     </div>
