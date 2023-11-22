d1981ab8184479f7c53d5c45ac36496dabc1bfc0
SapiS68 <cz.sapis@gmail.com>
2023-11-22 12:07:51 +0100

    create_question_table.php hozzáadva, migrációs fájlok átnevezése

---
+++ Projekt/database/migrations/create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->datetime('date')->primary();
+            $table->string('solution_id');
+            $table->integer('first_attempt');
+            $table->integer('second_attempt');
+            $table->integer('third_attempt');
+            $table->integer('fourth_attempt');
+            $table->integer('fifth_attempt');
+            $table->integer('missed');
+            $table->time('total_time');
+            $table->timestamps();
+
+            $table->foreign('solution_id')->references('solution_id')->on('solutions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
