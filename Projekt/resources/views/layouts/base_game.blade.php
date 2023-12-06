eaa95324907d00c55a6bdc6021a4c9c9442fd370
Törteli Márton <60424745+ewyyn7@users.noreply.github.com>
2023-12-06 12:21:35 +0100

    Game frontend felépítés.
    El lett különítve a kép nézegető rész és a tippelő rész

--- Projekt/resources/views/layouts/base_game.blade.php
+++ Projekt/resources/views/layouts/base_game.blade.php
@@ -7,28 +7,47 @@
 </head>
 <body class="bg-zinc-900">
 
-
     <div class="flex justify-center">
         <div class="w-full h-1/2 p-10 ">
         
             <div class="items-center justify-center">
                 <div class="bg-neutral-950 p-3 rounded-lg text-gray-50">
                         <h1 class="flex justify-center	text-transform: uppercase text-bold font-bold mb-1">Találd ki a játék címét a kép alapján.</h1>
-                        <div class="flex justify-center"> 
-                            <p lass="flex justify-center">A kép egy játékból származik. A segítségével írd be az ötletedet.</p>
-                        </div>                 
+                
                         <div class="flex justify-center p-5 m-5">
-                            <img class="w-1/3 h-1/3 rounded-lg"src="https://i.imgur.com/73kXBCz.jpeg" > 
+                            <img class="w-full h-full rounded-lg"src="https://i.imgur.com/73kXBCz.jpeg" > 
                             
                         </div> 
-                        <div class="flex justify-center"> 
-                            <input  class=" bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name="" type="text" placeholder="Ötleted helye." /> 
-                            <button class="text-slate-950 m-1 p-1 rounded bg-gradient-to-r from-indigo-700 to-gray-400 hover:from-indigo-800 hover:to-gray-600 font-semibold">Következő kép</button>
+                        <div class="flex justify-center">  
+                            <button class="text-slate-950 m-1 p-1 rounded bg-gradient-to-r from-indigo-700 to-gray-400 hover:from-indigo-800 hover:to-gray-600 font-semibold">Előző</button>
+                            <button class="text-slate-950 m-1 p-1 rounded bg-gradient-to-r from-indigo-700 to-gray-400 hover:from-indigo-800 hover:to-gray-600 font-semibold">Következő</button>
 
                         </div> 
                         <div class="flex justify-center"> 
-                            <p class="flex justify-center text-red-500">Red Dead Redemption 1 </p>
 
+
+                        </div> 
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full h-1/2 p-10 ">
+        
+            <div class="items-center justify-center">
+                <div class="bg-neutral-950 p-3 rounded-lg text-gray-50">
+                          <div class="flex justify-center"> 
+                            <p lass="flex justify-center">A kép egy játékból származik. A segítségével írd be az ötletedet.</p>
+                        </div>                 
+
+                        <div class="flex justify-center"> 
+                            <input  class=" bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name="" type="text" placeholder="Ötleted helye." /> 
+                            <button class="text-slate-950 m-1 p-1 rounded bg-gradient-to-r from-indigo-700 to-gray-400 hover:from-indigo-800 hover:to-gray-600 font-semibold">Beküldés</button>
+
+                        </div> 
+                        <div class="justify-center"> 
+                            <p class="flex justify-center text-xl text-red-500">Red Dead Redemption 1 </p>
+                            <p class="flex justify-center text-xl text-red-500">Far Crry </p>
+                            <p class="flex justify-center text-xl text-green-600">Euro Truck 2 </p>
                         </div> 
                 </div>
             </div>
@@ -36,6 +55,9 @@
 
 
     </div>
+
+    
+
 
     @yield('content')
 </body>
