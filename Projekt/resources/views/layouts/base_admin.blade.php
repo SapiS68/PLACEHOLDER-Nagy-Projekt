668771b3b7a5bc89dbe07551de76de105c042631
Törteli Márton <60424745+ewyyn7@users.noreply.github.com>
2023-12-09 20:51:56 +0100

    Admin oldal layouttá alakítás és admin view szerkesztése

---
+++ Projekt/resources/views/layouts/base_admin.blade.php
@@ -0,0 +1,44 @@
+
+
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body class="bg-zinc-900">
+
+
+    <div class="justify-center flex">
+        <div class="w-1/2 h-1/2 p-10 ">
+        
+            <div class="items-center justify-center">
+                <div class="bg-neutral-950 p-5 rounded-lg text-gray-50">
+                <div class="flex justify-center">
+                            <div class="justify-center">
+                                <h1 class="text-white text-center text-2xl">Rendszergazdakezelés</h1>
+                                <div class="flex-col justify-center">
+                                    <input class=" bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name="user" type="text" placeholder="Felhasználó"/>
+                                    <button class="text-slate-950 m-1 p-1 rounded bg-gradient-to-r from-indigo-700 to-gray-400 hover:from-indigo-800 hover:to-gray-600 font-semibold">Hozzáad</button>
+                                </div>
+                                <hr/>
+                            </div>
+               
+
+                        
+                        
+                        <div class="flex justify-center p-5 m-5">
+                            <img class="w-1/3 h-1/3 rounded-lg" >
+                        </div>  
+                        
+                </div>
+            </div>
+        </div>
+
+       
+
+    @yield('content')
+</body>
+
+</html>
