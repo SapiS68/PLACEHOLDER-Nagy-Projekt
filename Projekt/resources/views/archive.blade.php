1220c9f4b86a29982d0739aab614d1da9e03aa7a
Törteli Márton <60424745+ewyyn7@users.noreply.github.com>
2023-12-09 20:10:03 +0100

    Archívum oldal létrehozása
    archive.blade.php alap felépítés, naptár kinézet létrehozása.
    base_ar.blade.php-ban a layout használata és menüvel összeillesztés.

---
+++ Projekt/resources/views/archive.blade.php
@@ -0,0 +1,9 @@
+
+
+@extends('layouts.base_ar')
+@section('content')
+@stop 
+
+
+
+@extends('layouts.menu')
---
+++ Projekt/resources/views/layouts/base_ar.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body class="bg-zinc-900">
+
+
+    <div class="justify-center flex">
+        <div class="w-1/2 h-1/2 p-10 ">
+        
+            <div class="items-center justify-center">
+                <div class="bg-neutral-950 p-5 rounded-lg text-gray-50">
+                        <h1 class="flex justify-center	text-transform: uppercase text-bold font-bold mb-1">Archívum</h1>
+
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 01.</button>
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 02.</button>
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 03.</button>
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 04.</button>
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 05.</button>
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 06.</button>
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 07.</button>
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 08.</button>
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 09.</button>
+                        <button class="font-extrabold text-black text-2xl m-3 py-6 rounded bg-gradient-to-r from-green-50 to-gray-50 hover:from-green-50 hover:to-gray-50 font-semibold">2023 12. 10.</button>
+                        
+                        
+                        
+                        <div class="flex justify-center p-5 m-5">
+                            <img class="w-1/3 h-1/3 rounded-lg" >
+                        </div>  
+                        
+                </div>
+            </div>
+        </div>
+
+       
+
+    @yield('content')
+</body>
+
+</html>
