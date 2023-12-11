e47c29b6d8554e18931b44c464190bdc9be948f7
Törteli Márton <60424745+ewyyn7@users.noreply.github.com>
2023-12-11 12:26:07 +0100

    Add game kiegészítése
    Külön játék hozzáadás form hozzáadása

--- Projekt/resources/views/layouts/base_add_game.blade.php
+++ Projekt/resources/views/layouts/base_add_game.blade.php
@@ -37,6 +37,45 @@
                         <div class="flex justify-center">
                             <button class="flex justify-center text-slate-950 m-3 p-1 rounded bg-gradient-to-r from-green-500 to-emerald-500 hover:from-green-600 hover:to-emerald-600 font-semibold">Hozzáadás</button>
                         </div>
+
+                        
+                    <div class="flex-col justify-center">
+                        @foreach ($errors->all() as $error)
+                            <p class="w-full text-red-500">{{ $error }}</p>
+                        @endforeach
+                    </div> 
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="w-1/3 h-1/2 p-10 ">
+            <div>
+                <div class=" bg-neutral-950 p-3 rounded-lg text-gray-50">
+                    <h1 class="flex justify-center text-transform: uppercase text-bold font-bold mb-1">játék hozzáadása:</h1>
+
+                    <form class="" action="" method="POST">
+                        @csrf
+
+                        <input  class=" bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name=""  placeholder="Játék neve" />                 
+
+
+                        <p class="flex justify-center text-sm text-red-50">Borítókép </p>
+                        <input  class=" bg-gradient-to-r from-stone-700 to-neutral-500 p-1 rounded-lg text-zinc-50 placeholder:text-zinc-50" name="" type="file" /> 
+
+                        <div class="flex justify-center">
+                            <button class="flex justify-center text-slate-950 m-3 p-1 rounded bg-gradient-to-r from-green-500 to-emerald-500 hover:from-green-600 hover:to-emerald-600 font-semibold">Hozzáadás</button>
+                        </div>
+
+                        
+                    <div class="flex-col justify-center">
+                        @foreach ($errors->all() as $error)
+                            <p class="w-full text-red-500">{{ $error }}</p>
+                        @endforeach
+                    </div> 
                         
                     </form>
                 </div>
